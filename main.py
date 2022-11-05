3c36e46d47d47d04628ab1ee005f095d4d036245
Sarah Rodenbeck <30814237+srodenbeck@users.noreply.github.com>
2022-11-04 23:56:44 -0400

    add pickle

--- main.py
+++ main.py
@@ -5,21 +5,15 @@ from flask import Flask
 import pandas as pd
 
 app = Flask(__name__)
-
-def prep():
-    df = pd.read_csv('online_retail.csv')
-    df_baskets = df[['InvoiceNo', 'StockCode', 'Description', 'Quantity']]
-    df_items = df_baskets.pivot_table(index='InvoiceNo', columns=['Description'], values='Quantity').fillna(0)
-    return df_items
     
 @app.route('/')
 def get_recommendations(item='PARTY BUNTING'):
-    df_items = prep()
+    df_items = pd.read_pickle('items.pkl')
     recommendations = df_items.corrwith(df_items[item])
     recommendations.dropna(inplace=True)
     recommendations = pd.DataFrame(recommendations, columns=['correlation']).reset_index()
     recommendations = recommendations.sort_values(by='correlation', ascending=False)
-    return recommendations.to_dict()
+    return recommendations.head(10).to_dict()
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
